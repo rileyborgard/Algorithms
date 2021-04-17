54c9034c7de2a145f431edf521609b5033f4cda8
Riley Borgard <rileyborgard@gmail.com>
2021-04-17 16:26:15 -0400

    poly test

--- test/algebra/poly-test.cpp
+++ test/algebra/poly-test.cpp
@@ -8,31 +8,19 @@ using namespace std;
 const int M = 998244353;
 using base = mod<M>;
 
-// Bell Numbers:
-// exp(exp(x) - 1)
-
-// Stirling Numbers:
-// D(x) = e^x - 1
-// exp(y D(x))
-// coefficient of y^k
-// D(x)^k / k!
-// [x^n / n!] (1/k!) D(x)^k
-// (n!/k!) exp(k log D(x))
-
 int main() {
-    const int si = 100000;
-    poly<base> D;
-    D.resize(si);
+    const int si = 8;
+    poly<base> H;
+    H.resize(si);
     vector<base> fact(si);
     fact[0] = 1;
+    H.set(0, 1);
     for(int i = 1; i < si; i++) {
         fact[i] = fact[i - 1] * i;
-        D.set(i, fact[i].inv());
+        H.set(i, base(i).pow(i) / fact[i]);
     }
-    D = D.exp(si);
-    for(int i = 0; i < 10; i++) {
-        base x = D[i] * fact[i];
-        cout << x() << ' ';
+    poly<base> D = H.log(si);
+    for(int i = 1; i < si; i++) {
+        cout << i << ' ' << (D[i] * fact[i])() << '\n';
     }
-    cout << endl;
 }
