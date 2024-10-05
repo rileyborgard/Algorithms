b7decf045f356e19e39b33ac3585de4246600e3b
Riley Borgard <rileyborgard@gmail.com>
2024-10-05 10:57:49 -0700

    hash map

---
+++ source/data-structures/hash_map.h
@@ -0,0 +1,23 @@
+
+struct custom_hash {
+    static inline uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+template <typename T>
+struct hash_map : unordered_map<long long, T, custom_hash> {
+    T get(long long i) const {
+        auto it = this->find(i);
+        return it != this->end() ? it->second : T();
+    }
+};
