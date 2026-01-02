dd6199146ee9aaa7e7519dcc4aee8ec8d162acfb
Riley Borgard <rileyborgard@gmail.com>
2026-01-02 12:04:30 -0500

    Next array vector

--- include/utils/next_array.h
+++ include/utils/next_array.h
@@ -15,3 +15,22 @@ bool next_array(IterT begin, IterT end, T mx) {
     }
     return false;
 }
+
+template <typename IterT, typename T>
+bool next_array_vec(IterT begin, IterT end, const std::vector<T>& mx) {
+    IterT it = end;
+    auto mxit = mx.end();
+    while (it != begin) {
+        --it;
+        --mxit;
+        T val = *it;
+        ++val;
+        if (val == *mxit) {
+            *it = T();
+        } else {
+            *it = val;
+            return true;
+        }
+    }
+    return false;
+}
--- tests/test_next_array.cpp
+++ tests/test_next_array.cpp
@@ -1,4 +1,5 @@
 
+#include <vector>
 #include "utils/next_array.h"
 #include "gtest/gtest.h"
 
@@ -18,6 +19,32 @@ TEST(NextArrayTest, Basic) {
         {1, 0, 1},
         {1, 1, 0},
         {1, 1, 1},
+    };
+
+    ASSERT_EQ(res, expected);
+}
+
+TEST(NextArrayTest, MaxVector) {
+    std::vector<int> vec = {0, 0, 0};
+    std::vector<std::vector<int>> res;
+    std::vector<int> mx = {2, 3, 2};
+    do {
+        res.push_back(vec);
+    } while (next_array_vec(vec.begin(), vec.end(), mx));
+
+    std::vector<std::vector<int>> expected = {
+        {0, 0, 0},
+        {0, 0, 1},
+        {0, 1, 0},
+        {0, 1, 1},
+        {0, 2, 0},
+        {0, 2, 1},
+        {1, 0, 0},
+        {1, 0, 1},
+        {1, 1, 0},
+        {1, 1, 1},
+        {1, 2, 0},
+        {1, 2, 1},
     };
 
     ASSERT_EQ(res, expected);
