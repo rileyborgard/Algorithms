7b2b47b44a0c311e039288ef11325b90d62ce9f7
Riley Borgard <rileyborgard@gmail.com>
2026-04-25 16:21:56 +0000

    Fix warning of initialization order dinic

--- include/graphs/dinic.h
+++ include/graphs/dinic.h
@@ -9,7 +9,7 @@ struct dinic {
     vector<edge> e;
     vector<vector<int>> g;
     vector<int> level, p;
-    dinic(int n) : g(n), p(n), level(n) {}
+    dinic(int n) : g(n), level(n), p(n) {}
     void add_edge(int u, int v, C c) {
         int k = (int) e.size();
         e.push_back({v, c, 0});
